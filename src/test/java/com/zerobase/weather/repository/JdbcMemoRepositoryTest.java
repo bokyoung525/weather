9dcb8de8276eec27d7333418bd2416ec4e9f3206
Bokyoung Lee <leeqhrud525@gmail.com>
2023-12-02 01:25:36 +0900

    231202

---
+++ src/test/java/com/zerobase/weather/repository/JdbcMemoRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.zerobase.weather.repository;
+
+import com.zerobase.weather.domain.Memo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class JdbcMemoRepositoryTest {
+    @Autowired
+    JdbcMemoRepository jdbcMemoRepository;
+
+    @Test
+    void insertMemoTest() {
+        //given
+        Memo newMemo = new Memo(2, "insert memo test");
+
+        //when
+        jdbcMemoRepository.save(newMemo);
+
+        //then
+        Optional<Memo> result = jdbcMemoRepository.findById(2);
+        assertEquals(result.get().getText(), "insert memo test");
+    }
+
+    @Test
+    void findAllMemo() {
+        //given
+        List<Memo> memoList = jdbcMemoRepository.findAll();
+        //when
+        //then
+        assertNotNull(memoList);
+    }
+}
